0bb62f2458615bad2f35af834b8fb3565972385a
MeraStein <merasteinberger@gmail.com>
2025-07-26 22:49:00 +0300

    fixed merchaot

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import {TITLE, SUBTITLE, PARAGRAPHS} from "./global" 
+import { TITLE, SUBTITLE, PARAGRAPHS } from "./global"
 import Image from "next/image";
-import { useRouter } from "next/navigation"; 
+import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 import Paragraphs from "./components/paragraphs/Paragraphs";
 import Register from "./components/register/Register";
@@ -26,7 +26,7 @@ export default function Home() {
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhdh8ypYm1_agPI1Dy9837Ggm60HbcZPGmTA&s"
           alt="ילדים לומדים קוד"
         />
-        <img 
+        <img
           src="https://img.freepik.com/premium-vector/illustration-student-activity_1368420-4108.jpg?semt=ais_hybrid&w=740"
           alt="ילדים לומדים קוד"
         />
@@ -37,28 +37,28 @@ export default function Home() {
       </div>
 
       <div>
-        <Paragraphs/>
+        <Paragraphs />
       </div>
 
       <section className={styles.section}>
         <h2>🎯 מה מחכה לנו בסדנה?</h2>
         <ul>
           <li>✅לימוד עקרונות התכנות בשפת פייתון בדרך כיפית וברורה</li>
-          <li>✅ רכישת ידע באנגלית "על הדרך"</li>
+          <li>✅ רכישת ידע באנגלית &quot;על הדרך&quot;</li>
           <li>✅ פרויקטים איכותיים – משחקים ותוכנות</li>
           <li>✅ פיתוח חשיבה לוגית ויצירתית</li>
           <li>✅ אווירה נעימה ברוח יהודית וחסידית</li>
         </ul>
       </section>
 
-       <div className={styles.cta}>
-          {/* <button onClick={handleRegisterClick}>📩 רוצה שנחזור אליכם?</button> */}
-          {/* <h3>📩 רוצה שנחזור אליכם?</h3> */}
-          <Register/>
-        </div>
+      <div className={styles.cta}>
+        {/* <button onClick={handleRegisterClick}>📩 רוצה שנחזור אליכם?</button> */}
+        {/* <h3>📩 רוצה שנחזור אליכם?</h3> */}
+        <Register />
+      </div>
 
       <footer className={styles.footer}>
-       מספר המקומות מוגבל!
+        מספר המקומות מוגבל!
       </footer>
     </div>
   );
